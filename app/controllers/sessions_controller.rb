8c5873e779339748c06e2634cb02fbf87fdba236
Priyanka S. Ghate <priyanka.ghate@anchanto.com>
2016-10-22 19:00:25 +0530

    authorization success

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,25 +20,6 @@ class SessionsController < ApplicationController
   end
 
   def put_oauth_token
-  	  # p "====== inside put"
-  	  # p params
-     #  if(!params[:code].present?)
-     #    p "-----------#{params['client_id']}------------------------------"
-     #    params[:client_id]=params['client_id']
-     #    p "-----------#{params[:client_id]}------------------------------"
-     #    params[:client_secret]=params['client_secret']
-     #    redirect_to pages_path(params)
-     #  else
-     #    p "............Else..................."
-     #  p"#{params[:client_id]}--------#{params[:client_secret]}"
-     #    p "#{params[:code]}"
-     #    p "................................................"
-     #    req_params = "client_id=#{params[:client_id]}&client_secret=#{params[:client_secret]}&code=#{params[:code]}&grant_type=authorization_code&redirect_uri=#{ENV['oauth_redirect_uri']}"
-     #    response = JSON.parse RestClient.post("http://localhost:3000/oauth/token", req_params)
-     #    params[:access_token] = response['access_token']
-     #    #p "````````````#{params[:access_token]}`````````````````"
-     #     redirect_to pages_path(params)
-     # end
-
+  	  
   end
 end
